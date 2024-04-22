71389276ac4acdac9cf971428135dc0ce3063523
t-haruki122 m <231205122@ccmailg.meijo-u.ac.jp>
2024-04-22 10:46:54 +0900

    Add Some Error Handlings

--- exec_updater.py
+++ exec_updater.py
@@ -8,17 +8,15 @@ import datetime
 
 # ---------------------------------------------------------------
 
-"""
-Select the version of the server to download (ver)
+#Select the version of the server to download (ver)
 
-[Release]
-0 : Windows Release
-1 : Linux Release
+# [Release]
+# 0 : Windows Release
+# 1 : Linux Release
 
-[Preview]
-2 : Windows Preview
-3 : Linux Preview
-"""
+# [Preview]
+# 2 : Windows Preview
+# 3 : Linux Preview
 
 ver = 1
 
@@ -30,6 +28,7 @@ is_replace = True
 
 # ---------------------------------------------------------------
 
+
 def get_time():
     return datetime.datetime.now().strftime('%Y%m%d%H%M')
 
@@ -37,19 +36,29 @@ dl_filename = 'server_temp.zip'
 old_server_path = f'old_server_{get_time()}'
 backup_path = 'upd_backup'
 dl_path = backup_path + '/' + dl_filename
+times = 5
 
 RandNum = random.randint(1000, 9999)
 ua = f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.33 (KHTML, like Gecko) Chrome/90.0.{RandNum}.212 Safari/537.33"
 headers = {'User-Agent': ua}
 
+
 def get_latest_url():
+    global times
     url = "https://www.minecraft.net/en-us/download/server/bedrock"
-    response = requests.get(url, headers=headers, timeout = 10)
+    for i in range(times):
+        response = requests.get(url, headers=headers, timeout = 10)
+        if response.status_code == 200:
+            break
+        print(f"Failed to get the latest version! (retry {i+1}/{times})")
+    else:
+        raise RuntimeError("Failed to get the latest version! Please try again later.")
     print("response: ", response)
     content = str(response.text.encode('utf-8'))
     urls = serch_url(content, '<a href="https://minecraft.azureedge.net/', '.zip"')
     out = select_version(urls)
     return out
+
 
 def serch_url(text, st, fn):
     out, ans = [], ''
@@ -69,45 +78,61 @@ def serch_url(text, st, fn):
             ans += i
     return out
 
+
 def select_version(urls):
     global ver
     return urls[ver]
 
+
 def download_file(url):
-    global dl_path
-    urlData = requests.get(url).content
+    global dl_path, times
+    for i in range(times):
+        response = requests.get(url, headers=headers, timeout = 10)
+        if response.status_code == 200:
+            break
+        print(f"Failed to download! (retry {i+1}/{times})")
+    else:
+        raise RuntimeError("Failed to download! Please try again later.")
+    print("response: ", response)
+    urlData = response.content
 
     with open(dl_path, mode='wb') as f: # write binary
         f.write(urlData)
+
 
 def unzip_file():
     global dl_path
     shutil.unpack_archive(dl_path, now_server_path)
 
+
 def rename_old_file():
     os.rename(now_server_path, old_server_path)
+
 
 def backup_file():
     # backup worlds & allowlist.json & server.properties
     global old_server_path, backup_path
     try:
         shutil.copytree(f'{old_server_path}/worlds', f'{backup_path}/worlds')
-    except:
-        print("Cannot copy Worlds data! (Maybe it's empty)")
+    except FileNotFoundError as e:
+        print("Cannot copy Worlds data! (Maybe it's empty) :", e)
     shutil.copy(f'{old_server_path}/allowlist.json', f'{backup_path}/allowlist.json')
     shutil.copy(f'{old_server_path}/server.properties', f'{backup_path}/server.properties')
+
 
 def copy_new_file():
     global backup_path, now_server_path
     try:
         shutil.copytree(f'{backup_path}/worlds', f'{now_server_path}/worlds')
-    except:
-        print("Cannot copy Worlds data! (Maybe it's empty)")
+    except FileNotFoundError as e:
+        print("Cannot copy Worlds data! (Maybe it's empty) :", e)
     shutil.copy(f'{backup_path}/allowlist.json', f'{now_server_path}/allowlist.json')
     shutil.copy(f'{backup_path}/server.properties', f'{now_server_path}/server.properties')
 
+
 def is_now_server_exist(now_server_path):
     return os.path.exists(now_server_path)
+
 
 def check_now_version():
     # get version of now server
@@ -118,7 +143,7 @@ def check_now_version():
     for i in files_list:
         if i[:8] == "vanilla_":
             ver = i[8:].split(".")
-            for j in range(len(ver)):
+            for j in enumerate(ver):
                 if int(ver[j]) < maxi[j]:
                     break
                 if int(ver[j]) > maxi[j]:
@@ -129,6 +154,7 @@ def check_now_version():
     out = ".".join([str(i) for i in maxi])
     return out
 
+
 def is_update_available(url):
     global now_server_path
     now_version = check_now_version()
@@ -136,6 +162,7 @@ def is_update_available(url):
     print("*\nnow_version: ", now_version)
     print("latest_version: ", latest_version)
     return now_version != latest_version
+
 
 test = True
 if __name__ == '__main__' and test:
@@ -157,19 +184,19 @@ if __name__ == '__main__' and test:
         else:
             print("Update is available!")
 
-    print("*\nDownloading...")
-    try:
-        download_file(url)
-    except:
-        print("Download failed! Please try again later.")
-        exit()
-    print("Downloaded!")
-
     if not os.path.exists(backup_path):
         os.mkdir(backup_path)
         print("backup (temporary) directory created!")
     else:
         print("backup (temporary) directory found!")
+
+    print("*\nDownloading...")
+    try:
+        download_file(url)
+    except RuntimeError as e:
+        print("Download failed! Please try again later.", e)
+        exit()
+    print("Downloaded!")
 
     if is_replace:
         rename_old_file()
